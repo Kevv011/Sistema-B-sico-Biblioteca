26de10d12a948f968a5a7a7bbb0ac7a970c3efe4
Kevin Arevalo <kevinarepalacios123@gmail.com>
2024-11-28 08:30:16 -0600

    Inicio de proyecto biblioteca: Funcionalidad de simulacion de un Login

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+require "./public/login_process.php"; //Funcionamiento de simulacion de LOGIN
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar Sesión</title>
+
+    <!--CSS BOOTSTRAP-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <!--CDN FONTAWESOME-->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <main>
+
+        <!--TITULO-->
+        <section>
+            <h1>Iniciar Sesión </h1>
+        </section>
+
+        <!--FORM DE INICIO DE SESION-->
+        <form action="" method="post">
+            <input type="text" name="username" placeholder="Usuario" required>
+            <input type="password" name="password" placeholder="Contraseña" required>
+
+            <button type="submit">Iniciar Sesión</button>
+        </form>
+    </main>
+
+
+
+    <!--JS BOOTSTRAP-->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ public/login_process.php
@@ -0,0 +1,15 @@
+<?php
+
+session_start();
+
+if(isset($_POST['username'], $_POST['password'])) {
+
+    //Capturas la informacion y almacenarla en la sesion
+    $_SESSION['$username'] = trim($_POST['username']);
+    $_SESSION['$password'] = trim($_POST['password']);
+
+    //Dirigirse a la pagina de INICIO de la biblioteca
+    header('location: inicio.php');
+}
+
+?>
