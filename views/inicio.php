565396bb73351964a19f42d770859a892e4021b6
Kevin Arevalo <kevinarepalacios123@gmail.com>
2024-11-30 17:02:43 -0600

    Inicio de form para libros

--- views/inicio.php
+++ views/inicio.php
@@ -7,6 +7,14 @@ if(isset($_SESSION['libros'])) {
     $_SESSION['libros'] = [];
 }
 
+//Obtencion de las variables de los campos con POST
+if(isset($_POST['book'], $_POST['autor'], $_POST['category'])) {
+  
+  $nombreLibro = $_POST['book'];
+  $nombreAutor = $_POST['autor'];
+  $nombreCategoria = $_POST['category'];
+}
+
 
 
 
@@ -14,24 +22,91 @@ if(isset($_SESSION['libros'])) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio Biblioteca</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Inicio Biblioteca</title>
 </head>
+
 <body>
 
-    <main>
+  <main>
 
-        <!--TITULO-->
-        <section>
-            <h1>BIBLIOTECH</h1>
-        </section>
+    <!--TITULO-->
+    <section>
+      <h1>BIBLIOTECH</h1>
+    </section>
 
-        <!--TABLA DE REGISTRO DE LIBROS Y FILTROS DE BUSQUEDA-->
+    <!--TABLA DE REGISTRO DE LIBROS Y FILTROS DE BUSQUEDA-->
 
+
+  </main>
+
+  <!-- Button trigger modal -->
+  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addLibro">
+    Agregar Libro
+  </button>
+
+  <!-- Modal con FORM para agregar un libro-->
+  <div class="modal fade" id="addLibro" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Nuevo Libro</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+
+        <!--FORM PARA INGRESAR UN LIBRO-->
+        <div class="modal-body">
+          <form action="" method="post">
+
+          <div class="col my-2 mx-3">
+            
+            <!--LIBRO-->
+            <div class="row mb-4">
+              <label class="h5">Nombre del libro:</label>
+              <input class="form-control" type="text" name="autor" placeholder="Los viajes de Gulliver..." required>
+            </div>
+
+            <!--AUTOR-->
+            <div class="row mb-4">
+              <label class="h5">Nombre del autor:</label>
+
+              <select class="form-select" name="autor" required>
+                <option selected>Selecciona un autor</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+            </div>
+            
+            <!--CATEGORIA-->
+            <div class="row mb-4">
+              <label class="h5">Categoria:</label>
+
+              <select class="form-select" name="category" required>
+                <option selected>Selecciona una categoria</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+            </div>
+          </div>
+
+          <!--BOTONES-->
+          <div class="buttons text-center">
+            <button class="btn btn-primary">Agregar</button>
+          </div>
+            
+          </form>
+          
+        </div>
         
-    </main>
-    
+      </div>
+    </div>
+  </div>
+
 </body>
+
 </html>
